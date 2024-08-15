06c5a70edb77e006eff4e2583dcd89cfd307ea39
henryzord <henry.cagnini@gmail.com>
2024-08-15 12:15:29 -0300

    fazendo o básico

--- dataBase.py
+++ dataBase.py
@@ -2,7 +2,8 @@ import mysql.connector
 from datetime import date, time, datetime
 from mysql.connector import Error
 
-class Conection:
+
+class Connection(object):
     def __init__(self, host_name, user_name, user_password, db_name):
         self.host_name = host_name
         self.user_name = user_name
@@ -15,30 +16,30 @@ class Conection:
             database=self.db_name
         )
         self.cursor = self.connection.cursor()
-        
+
     def create_database(self):
         try:
             # Conectar ao servidor MySQL
             if self.connection.is_connected():
                 # Criar banco de dados
-                self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.db_name}")
+                self.cursor.execute(f'''CREATE DATABASE IF NOT EXISTS {self.db_name}''')
                 print(f"Banco de dados '{self.db_name}' criado com sucesso!")
         except Error as e:
             print(f"Erro ao criar o banco de dados: {e}")
-            
+
     def create_tables(self):
         try:
             # Conectar ao banco de dados MySQL
             if self.connection.is_connected():
                 # Criar uma tabela
-                create_table_query = f"""
+                create_table_query = f'''
                 CREATE TABLE IF NOT EXISTS Usuarios (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     nome VARCHAR(100) NOT NULL
-                )"""
+                )'''
                 self.cursor.execute(create_table_query)
                 print(f"Tabela Usuarios criada com sucesso no banco de dados '{self.db_name}'!")
-                create_table_query = f"""
+                create_table_query = f'''
                 CREATE TABLE IF NOT EXISTS Contas (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     banco VARCHAR(100) NOT NULL,
@@ -47,10 +48,10 @@ class Conection:
                     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     id_usuario INT,
                     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id) ON DELETE CASCADE
-                )"""
+                )'''
                 self.cursor.execute(create_table_query)
                 print(f"Tabela Contas criada com sucesso no banco de dados '{self.db_name}'!")
-                create_table_query = f"""
+                create_table_query = f'''
                 CREATE TABLE IF NOT EXISTS Rotinas (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     tipo VARCHAR(1) NOT NULL,
@@ -62,10 +63,10 @@ class Conection:
                     descricao VARCHAR(300),
                     id_conta INT,
                     FOREIGN KEY (id_conta) REFERENCES Contas(id) ON DELETE CASCADE
-                )"""
+                )'''
                 self.cursor.execute(create_table_query)
                 print(f"Tabela Rotinas criada com sucesso no banco de dados '{self.db_name}'!")
-                create_table_query = f"""
+                create_table_query = f'''
                 CREATE TABLE IF NOT EXISTS Historico (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -74,21 +75,21 @@ class Conection:
                     descricao VARCHAR(300),
                     id_conta INT,
                     FOREIGN KEY (id_conta) REFERENCES Contas(id) ON DELETE CASCADE
-                )"""
+                )'''
                 self.cursor.execute(create_table_query)
-                print(f"Tabela Historico criada com sucesso no banco de dados '{self.db_name}'!")     
+                print(f"Tabela Historico criada com sucesso no banco de dados '{self.db_name}'!")
         except Error as e:
             print(f"Erro ao criar a tabela: {e}")
 
-    def __insert(self, table:str, columns:str, values:str):
+    def __insert(self, table: str, columns: str, values: str):
         try:
             # Conectar ao banco de dados MySQL
             if self.connection.is_connected():
                 # Inserir um novo usuário
-                insert_user_query = """
+                insert_user_query = '''
                 INSERT INTO {0} ({1})
                 VALUES ({2});
-                """.format(table, columns, values)
+                '''.format(table, columns, values)
                 self.cursor.execute(insert_user_query)
                 self.connection.commit()  # Confirmar a transação
             print(f"'{values}' inserido com sucesso na tabela '{table}'!")
@@ -97,7 +98,7 @@ class Conection:
             print(f"Erro ao inserir {table}: {e}")
             return False
 
-    def __select(self, table:str, columns:str, where:str):
+    def __select(self, table: str, columns: str, where: str):
         try:
             # Conectar ao banco de dados MySQL
             if self.connection.is_connected():
@@ -111,7 +112,7 @@ class Conection:
             print(f"Erro no select {table}: {e}")
             return False
 
-    def __delete(self, table:str, where:str):
+    def __delete(self, table: str, where: str):
         try:
             # Conectar ao banco de dados MySQL
             if self.connection.is_connected():
@@ -125,39 +126,43 @@ class Conection:
             print(f"Erro ao remover {table}: {e}")
             return False
 
-    def create_user(self, nome:str):
+    def create_user(self, nome: str):
         return self.__insert(table="Usuarios", columns="nome", values="'{0}'".format(nome))
-            
-    def search_user(self, columns:str = "*", where:str = "1=1"):
+
+    def search_user(self, columns: str = "*", where: str = "1=1"):
         return self.__select(table="Usuarios", columns=columns, where=where)
 
-    def remove_user(self, where:str = "1=1"):
+    def remove_user(self, where: str = "1=1"):
         self.__delete(table="Usuarios", where=where)
 
-    def create_account(self, bank:str, typ:str, date:datetime, idUser:int, money:float=0.0):
+    def create_account(self, bank: str, typ: str, date: datetime, idUser: int, money: float = 0.0):
         return self.__insert(
-            table="Contas", 
+            table="Contas",
             columns="banco, tipo, saldo, data_criacao, id_usuario",
             values="'{0}', '{1}', {2}, '{3}', {4}".format(bank, typ, money, date, idUser)
-            )
+        )
 
-    def search_account(self, columns:str = "*", where:str = "1=1"):
+    def search_account(self, columns: str = "*", where: str = "1=1"):
         return self.__select(table="Contas", columns=columns, where=where)
-    
-    def remove_account(self, where:str = "1=1"):
+
+    def remove_account(self, where: str = "1=1"):
         self.__delete(table="Contas", where=where)
 
-    def create_routines(self, typ:str, typR:str, value:float, iniDate:datetime, date:datetime, desc:str, idConta:int):
+    def create_routines(
+        self, typ: str, typR: str, value: float, iniDate: datetime, date: datetime, desc: str, idConta: int
+    ):
         return self.__insert(
-            table="Rotinas", 
+            table="Rotinas",
             columns="tipo, tipo_repeticao, valor, data_base, data_criacao, foi_add_manual, descricao, id_conta",
-            values="'{0}', '{1}', {2}, '{3}', '{4}', {5}, '{6}', {7}".format(typ, typR, value, iniDate, date, 0, desc, idConta)
-            )
+            values="'{0}', '{1}', {2}, '{3}', '{4}', {5}, '{6}', {7}".format(typ, typR, value, iniDate, date, 0, desc,
+                                                                             idConta
+                                                                             )
+        )
 
-    def search_routines(self, columns:str = "*", where:str = "1=1"):
+    def search_routines(self, columns: str = "*", where: str = "1=1"):
         return self.__select(table="Rotinas", columns=columns, where=where)
-    
-    def remove_routines(self, where:str = "1=1"):
+
+    def remove_routines(self, where: str = "1=1"):
         self.__delete(table="Rotinas", where=where)
 
     def close_connection(self):
@@ -165,6 +170,7 @@ class Conection:
             self.cursor.close()
             self.connection.close()
             print("Conexão ao MySQL encerrada.")
+
 
 if __name__ == "__main__":
     # Parâmetros de conexão
@@ -174,13 +180,13 @@ if __name__ == "__main__":
     db_name = "Financas"
 
     # Chamar a função para criar o banco de dados
-    connect = Conection(host_name, user_name, user_password, db_name)
+    connect = Connection(host_name, user_name, user_password, db_name)
     # connect.create_database()
     # connect.create_tables()
     r = connect.create_user(nome="Tales")
     r = connect.search_user(where="nome = 'Tales'")
     idUser = r[0][0]
-    
+
     for a in r:
         print("id: {0} nome: {1}".format(a[0], a[1]))
 
@@ -190,18 +196,18 @@ if __name__ == "__main__":
         print("id: {0} nome: {1}".format(a[0], a[1]))
     idConta = r[0][0]
     r = connect.create_routines(
-        typ="I", 
-        typR="M", 
-        value=1120, 
-        iniDate=datetime(2024, 8, 14), 
-        date=datetime.today(), 
+        typ="I",
+        typR="M",
+        value=1120,
+        iniDate=datetime(2024, 8, 14),
+        date=datetime.today(),
         desc="Salario de estágio TCU",
         idConta=idConta
     )
     r = connect.search_routines()
     for a in r:
         print("id: {0} Tipo: {1} Valor {2}".format(a[0], a[1], a[3]))
-        
+
     connect.remove_routines("id_conta = {0}".format(idConta))
     connect.remove_account("id_usuario = {0}".format(idUser))
     connect.remove_user("nome = 'Tales'")
